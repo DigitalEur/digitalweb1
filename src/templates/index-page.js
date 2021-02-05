4972b6d13ab3344dd189e6e7d24b16ffb16dd3fa
dev <europadigital.web@gmail.com>
2021-02-05 11:21:37 +0100

    Fix hamburger menu

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -8,9 +8,7 @@ import BlogRoll from '../components/BlogRoll'
 
 import "../assets/vendor/nucleo/css/nucleo.css";
 import "../assets/vendor/font-awesome/css/font-awesome.min.css";
-import "../assets/scss/argon-design-system-react.scss?v1.1.0";
-
-
+//import "../assets/scss/argon-design-system-react.scss?v1.1.0";
 // nodejs library that concatenates classes
 // reactstrap components
 import {Badge, Button, Card, CardBody, CardImg, Col, Container, FormGroup, Input, Row} from "reactstrap";
@@ -39,39 +37,39 @@ export const IndexPageTemplate = ({
                             </div>
                         </Col>
                         <Col className="order-lg-1" lg="6">
-                                <h1 className="display-3 text-white">
-                                    A beautiful Design System{" "}
-                                    <span>completed with examples</span>
-                                </h1>
-                                <p className="lead text-white">
-                                    The design system comes with four pre-built pages to
-                                    help you get started faster. You can change the text and
-                                    images and you're good to go.
-                                </p>
-                                <div className="btn-wrapper">
-                                    <Button
-                                        className="btn-icon mb-3 mb-sm-0"
-                                        color="info"
-                                        href="https://demos.creative-tim.com/argon-design-system-react/#/documentation/alerts?ref=adsr-landing-page"
-                                    >
+                            <h1 className="display-3 text-white">
+                                A beautiful Design System{" "}
+                                <span>completed with examples</span>
+                            </h1>
+                            <p className="lead text-white">
+                                The design system comes with four pre-built pages to
+                                help you get started faster. You can change the text and
+                                images and you're good to go.
+                            </p>
+                            <div className="btn-wrapper">
+                                <Button
+                                    className="btn-icon mb-3 mb-sm-0"
+                                    color="info"
+                                    href="https://demos.creative-tim.com/argon-design-system-react/#/documentation/alerts?ref=adsr-landing-page"
+                                >
                           <span className="btn-inner--icon mr-1">
                             <i className="fa fa-code" />
                           </span>
-                                        <span className="btn-inner--text">Components</span>
-                                    </Button>
-                                    <Button
-                                        className="btn-white btn-icon mb-3 mb-sm-0 ml-1"
-                                        color="default"
-                                        href="https://www.creative-tim.com/product/argon-design-system-react?ref=adsr-landing-page"
-                                    >
+                                    <span className="btn-inner--text">Components</span>
+                                </Button>
+                                <Button
+                                    className="btn-white btn-icon mb-3 mb-sm-0 ml-1"
+                                    color="default"
+                                    href="https://www.creative-tim.com/product/argon-design-system-react?ref=adsr-landing-page"
+                                >
                           <span className="btn-inner--icon mr-1">
                             <i className="ni ni-cloud-download-95" />
                           </span>
-                                        <span className="btn-inner--text">
+                                    <span className="btn-inner--text">
                             Download React
                           </span>
-                                    </Button>
-                                </div>
+                                </Button>
+                            </div>
                         </Col>
                     </Row>
                 </Container>
