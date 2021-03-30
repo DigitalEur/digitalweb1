4a49cec8d33a8cfb139caf05ac99da597aded808
dev <europadigital.web@gmail.com>
2021-03-30 18:22:34 +0200

    Add content on the bottom of homepage

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -403,47 +403,37 @@ export const IndexPageTemplate = ({
                 <Container className="pt-lg pb-300">
                     <Row className="text-center justify-content-center">
                         <Col lg="10">
-                            <h2 className="display-3 text-white">Costruiamo qualcosa insieme</h2>
+                            <h2 className="display-3 text-white">Cresci insieme a noi perché</h2>
+                            <h2 className="display-3 text-white"><b>IL DIGITALE RESTERÀ</b></h2>
                             <p className="lead text-white">
-                                According to the National Oceanic and Atmospheric
-                                Administration, Ted, Scambos, NSIDClead scentist, puts the
-                                potentially record low maximum sea ice extent tihs year down
-                                to low ice.
+                                Secondo uno studio del Cisco & IMD Glogbal Center for Business trasformation,
+                                nei prossimi 5 anni, 4 società su 10 falliranno o perderanno la loro leadership di
+                                mercato a causa di una mancata digitalizzazione del loro modello di business.
                             </p>
                         </Col>
                     </Row>
                     <Row className="row-grid mt-5">
-                        <Col lg="4">
+                        <Col lg="6">
                             <div
                                 className="icon icon-lg icon-shape bg-gradient-white shadow rounded-circle text-primary">
                                 <i className="ni ni-settings text-primary" />
                             </div>
-                            <h5 className="text-white mt-3">Building tools</h5>
+                            <h5 className="text-white mt-3">Strumenti di costruzione</h5>
                             <p className="text-white mt-3">
-                                Some quick example text to build on the card title and make
-                                up the bulk of the card's content.
+                                Gli strumenti del web marketing permettono di costruire una presenza online del proprio
+                                business, che aumenterà la notorietà della propria attività e del proprio brand.
                             </p>
                         </Col>
-                        <Col lg="4">
+                        <Col lg="6">
                             <div
                                 className="icon icon-lg icon-shape bg-gradient-white shadow rounded-circle text-primary">
                                 <i className="ni ni-ruler-pencil text-primary" />
                             </div>
-                            <h5 className="text-white mt-3">Grow your market</h5>
+                            <h5 className="text-white mt-3">Fai crescere il tuo mercato</h5>
                             <p className="text-white mt-3">
-                                Some quick example text to build on the card title and make
-                                up the bulk of the card's content.
-                            </p>
-                        </Col>
-                        <Col lg="4">
-                            <div
-                                className="icon icon-lg icon-shape bg-gradient-white shadow rounded-circle text-primary">
-                                <i className="ni ni-atom text-primary" />
-                            </div>
-                            <h5 className="text-white mt-3">Launch time</h5>
-                            <p className="text-white mt-3">
-                                Some quick example text to build on the card title and make
-                                up the bulk of the card's content.
+                                Sito web, presenza digitale, pubblicità online. In base alle tue ambizioni e modello di
+                                business, con noi, potrai accedere agli strumenti digitali necessari a far crescere la
+                                tua attività.
                             </p>
                         </Col>
                     </Row>
